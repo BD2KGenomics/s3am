5ebaa71d28891df31070060c56479d211aa5de2b
Hannes Schmidt <hannes@ucsc.edu>
2015-02-17 19:14:03 -0800

    Added Python version check to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup, find_packages
+import sys
+
+from setuptools import setup
+
+assert sys.version_info >= (2, 7)
 
 setup(
     name="s3am",
@@ -8,7 +12,7 @@ setup(
             's3am = s3am:main'
         ]
     },
-    py_modules = [ 's3am', 'humanize' ],
+    py_modules=[ 's3am', 'humanize' ],
     install_requires=[ 'pycurl', 'boto' ],
     tests_require=[ 'pyftpdlib' ]
 )
