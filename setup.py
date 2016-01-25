64a5b3e3e3602ffd4db03d671dee83131a72dc35
Hannes Schmidt <hannes@ucsc.edu>
2016-01-25 00:09:21 -0800

    Fix dependency declaration in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ kwargs = dict(
     tests_require=[
         'pytest==2.8.5',
         'pyftpdlib==1.5.0',
-        'bd2k-python-lib=1.13.dev10' ] )
+        'bd2k-python-lib==1.13.dev10' ] )
 
 from setuptools.command.test import test as TestCommand
 
